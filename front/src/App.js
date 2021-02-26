daf7dce7aa0be9b016d4f9692ce3d6076a4554b9
motta1616 <ingeniero.leonardomotta@gmail.com>
2021-02-26 16:53:19 -0500

    se agregan las dendencias de rudux

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,5 @@
 import React, { useContext, useReducer, useEffect, useRef, useState, createContext } from 'react';
+import reducer from "./reducer/reducer"
 
 const HOST_API = "http://localhost:8080/api";
 const initialState = {
@@ -150,43 +151,6 @@ const List = () => {
 }
 
 
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'update-item':
-      const todoUpItem = state.todo;
-      const listUpdateEdit = todoUpItem.list.map((item) => {
-        if (item.id === action.item.id) {
-          return action.item;
-        }
-        return item;
-      });
-      todoUpItem.list = listUpdateEdit;
-      todoUpItem.item = {};
-      return { ...state, todo: todoUpItem }
-    case 'delete-item':
-      const todoUpDelete = state.todo;
-      const listUpdate = todoUpDelete.list.filter((item) => {
-        return item.id !== action.id;
-      });
-      todoUpDelete.list = listUpdate;
-      return { ...state, todo: todoUpDelete }
-    case 'update-list':
-      const todoUpList = state.todo;
-      todoUpList.list = action.list;
-      return { ...state, todo: todoUpList }
-    case 'edit-item':
-      const todoUpEdit = state.todo;
-      todoUpEdit.item = action.item;
-      return { ...state, todo: todoUpEdit }
-    case 'add-item':
-      const todoUp = state.todo.list;
-      todoUp.push(action.item);
-      return { ...state, todo: {list: todoUp, item: {}} }
-    default:
-      return state;
-  }
-}
 
 const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
 import './index.css';
 import App from './App';
+import reducer from "./reducer/reducer"
+const store = createStore(reducer)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
