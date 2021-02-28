a04cd6ecaf8683a2c4b5b7f94ecc2bef207df430
motta1616 <ingeniero.leonardomotta@gmail.com>
2021-02-28 13:42:17 -0500

    se modifica para implementar el repositorio JPA

--- back/src/main/java/co/com/sofka/crud/TodoRepository.java
+++ back/src/main/java/co/com/sofka/crud/TodoRepository.java
@@ -1,6 +1,7 @@
 package co.com.sofka.crud;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
