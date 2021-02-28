89f242f83bc2fa7c724a71442de6990c452898ec
motta1616 <ingeniero.leonardomotta@gmail.com>
2021-02-27 22:52:47 -0500

    Se modifica el diseño grafico

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext, useReducer, useEffect, useRef, useState, createContext } from 'react';
-import reducer from "./reducer/reducer"
+import reducer from "./reducers/reducer"
 
 const HOST_API = "http://localhost:8080/api";
 const initialState = {
@@ -65,17 +65,20 @@ const Form = () => {
   }
 
   return <form ref={formRef}>
+    <div className="input-group mb-3">
     <input
       type="text"
       name="name"
       placeholder="¿Qué piensas hacer hoy?"
+      className="form-control" aria-describedby="button-addon1"
       defaultValue={item.name}
       onChange={(event) => {
         setState({ ...state, name: event.target.value })
-      }}  ></input>
-    {item.id && <button onClick={onEdit}>Actualizar</button>}
-    {!item.id && <button onClick={onAdd}>Crear</button>}
-  </form>
+      }} ></input>
+    {item.id && <button onClick={onEdit} className="btn btn-outline-secondary" type="button" id="button-addon1">Actualizar</button>}
+    {!item.id && <button onClick={onAdd} className="btn btn-outline-secondary" type="button" id="button-addon1">Crear</button>}
+    </div>
+    </form>
 }
 
 
@@ -127,22 +130,25 @@ const List = () => {
     textDecoration: 'line-through'
   };
   return <div>
-    <table >
+    <table className="table">
       <thead>
         <tr>
-          <td>ID</td>
-          <td>Tarea</td>
-          <td>¿Completado?</td>
+          <th scope="col" className="text-center">ID</th>
+          <th scope="col" className="text-center">Tarea</th>
+          <th scope="col" className="text-center">¿Completado?</th>
+          <th scope="col" className="text-center">Acción</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className="table-info">
         {currentList.map((todo) => {
           return <tr key={todo.id} style={todo.completed ? decorationDone : {}}>
-            <td>{todo.id}</td>
-            <td>{todo.name}</td>
-            <td><input type="checkbox" defaultChecked={todo.completed} onChange={(event) => onChange(event, todo)}></input></td>
-            <td><button onClick={() => onDelete(todo.id)}>Eliminar</button></td>
-            <td><button onClick={() => onEdit(todo)}>Editar</button></td>
+            <td className="text-center">{todo.id}</td>
+            <td className="text-center">{todo.name}</td>
+            
+            <td className="text-center"><input type="checkbox" defaultChecked={todo.completed} onChange={(event) => onChange(event, todo)}></input></td>
+            
+            <td className="text-center"><button onClick={() => onDelete(todo.id)} type="button" className="btn btn-primary col-4 mx-auto">Eliminar</button>
+            <button onClick={() => onEdit(todo)} type="button" className="btn btn-danger col-4 mx-auto">Editar</button></td>
           </tr>
         })}
       </tbody>
@@ -150,23 +156,27 @@ const List = () => {
   </div>
 }
 
-
-
 const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return <Store.Provider value={{ state, dispatch }}>
     {children}
   </Store.Provider>
-
 }
 
 function App() {
   return <StoreProvider>
-    <h3>To-Do List</h3>
-    <Form />
-    <List />
+  <div className="container">
+    <div className="jumbotron bg-success">
+      <h1 className="display-3 text-center">Lista de tareas</h1>
+    </div>
+    <div className="row"> 
+      <div className="col">
+        <Form />
+        <List />
+      </div>
+    </div>
+  </div>  
   </StoreProvider>
 }
-
 export default App;
--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import {createStore} from 'redux';
 import './index.css';
 import App from './App';
-import reducer from "./reducer/reducer"
+import reducer from "./reducers/reducer"
+
 const store = createStore(reducer)
 
 ReactDOM.render(
